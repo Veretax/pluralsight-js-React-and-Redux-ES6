8d10bf20e0526b634e64e1c7b78740513a48879a
Tim Western <veretax@gmail.com>
2018-12-28 21:45:11 -0500

    Add connect, mapstatetoprops and updateonClickSave for CoursesPage

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,5 +1,7 @@
 import React, { PropTypes } from 'react';
+import {connect} from 'react-redux';
 import Header from '../common/Header';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
@@ -20,7 +22,7 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    alert(`Saving ${this.state.course.title}`);
+    this.props.dispatch(courseActions.createCourse(this.state.course));
   }
 
   render() {
@@ -42,4 +44,11 @@ class CoursesPage extends React.Component {
   }
 }
 
-export default CoursesPage;
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+
+export default connect(mapStateToProps)(CoursesPage);
